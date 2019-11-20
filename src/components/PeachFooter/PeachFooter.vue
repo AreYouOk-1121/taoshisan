9d20cdf4f244645099102bad09ff10c4f622e637
wang nan nan <w17602n266102n@163.com>
2019-11-21 00:43:39 +0800

    footer

--- src/components/PeachFooter/PeachFooter.vue
+++ src/components/PeachFooter/PeachFooter.vue
@@ -2,7 +2,7 @@
   <div id="footerGuideContainer">
     <div class="guideItem" :class="{active:$route.path === '/msite'}" @click="goPath('/peachhome')">
       <span>
-        <i class="iconfont icon-shouye2"></i>
+        <img src="https://img.alicdn.com/imgextra/i1/2053469401/O1CN01IDoKYf2JJhy7Pld17_!!2053469401.png" alt="图片">  
       </span>
       <span>
         首页
@@ -10,7 +10,7 @@
     </div>
     <div class="guideItem" :class="{active:$route.path === '/order'}" @click="goPath('/peachpostage')">
       <span>
-        <i class="iconfont icon-sousuo"></i>
+        <img src="https://img.alicdn.com/imgextra/i2/2053469401/O1CN01IoDVRW2JJhy7uYnhS_!!2053469401.png" alt="图片">
       </span>
       <span>
         9.9包邮
@@ -18,7 +18,7 @@
     </div>
     <div class="guideItem" :class="{active:$route.path === '/profile'}" @click="goPath('/peachclassify')">
       <span>
-        <i class="iconfont icon-nav-order"></i>
+        <img src="https://img.alicdn.com/imgextra/i4/2053469401/O1CN01Ts9O8t2JJhy5zKLZn_!!2053469401.png" alt="图片">
       </span>
       <span>
         分类
@@ -26,7 +26,7 @@
     </div>
     <div class="guideItem" :class="{active:$route.path === '/profile'}" @click="goPath('/peachcollect')">
       <span>
-        <i class="iconfont icon-nav-order"></i>
+        <img src="https://img.alicdn.com/imgextra/i3/2053469401/O1CN01090Eiz2JJhy5zJoLf_!!2053469401.png" alt="图片">
       </span>
       <span>
         收藏
@@ -34,7 +34,7 @@
     </div>
     <div class="guideItem" :class="{active:$route.path === '/search'}" @click="goPath('/peachpersonal')">
       <span>
-        <i class="iconfont icon-wode"></i>
+        <img src="https://img.alicdn.com/imgextra/i1/2053469401/O1CN015lCWYO2JJhy8Mlr01_!!2053469401.png" alt="图片">
       </span>
       <span>
         我的
@@ -55,6 +55,27 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus" scoped>
-
+  #footerGuideContainer
+    background-color #fff
+    position fixed
+    left 0
+    bottom 0
+    width 100%
+    height 50px
+    border-top 1px solid #cccccc
+    box-sizing border-box
+    display flex
+    .guideItem
+      width 25%
+      height 100%
+      text-align center
+      display flex
+      flex-direction column
+      span
+        img
+          width 24px
+          height 24px
+        &:first-child
+          margin 5px 0 4px 0
  
 </style>
